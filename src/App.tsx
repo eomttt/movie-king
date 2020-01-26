259f175313cfdb1eb3d01c0a052854a664dcf712
eomttt <hyunt0413@gmail.com>
2020-01-26 14:29:25 +0900

    Add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import './App.css';
 
-interface IProps {
-  test: string;
-}
-
-const App: React.FunctionComponent<IProps> = ({ test }) => {
+const App = () => {
   return <div className="App" />;
 };
 
