e562925c67c8f6661689fde7cdaeb068f0009020
eomttt <hyunt0413@gmail.com>
2020-01-26 14:44:32 +0900

    Fix dir structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,11 +5,13 @@ import { mobileOffSet } from '../styles';
 
 const Container = styled.div`
   width: 100%;
+  display: flex;
 `;
 
 const Content = styled.div`
   width: 100%;
   max-width: ${mobileOffSet}px;
+  margin: 0 auto;
 `;
 
 interface IProps {
