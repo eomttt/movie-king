7030b01bd43de0c712ac687ccbdedd3b64c6a191
eomttt <hyunt0413@gmail.com>
2020-03-12 09:50:02 +0900

    feature: Test apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+
+import ApolloClient, { gql } from 'apollo-boost';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4000/graphql'
+      : '',
+});
+
+// For test movie query
+client
+  .query({
+    query: gql`
+      query {
+        testMovies(limit: 3, rating: 9) {
+          id
+          title
+          rating
+        }
+        testMovie(id: 15553) {
+          title
+          summary
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
