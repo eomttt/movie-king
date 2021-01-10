736c88a9972e79774be03ca15bfaf2968f0d6b89
eomttt <hyunt0413@gmail.com>
2021-01-10 14:48:04 +0900

    Delete GQL_DEV env

--- src/hooks/useBoxOffice.ts
+++ src/hooks/useBoxOffice.ts
@@ -8,7 +8,7 @@ const mock = () => dummy.boxOffice;
 export const useBoxOffice = () => {
   const {
     isLoading, isError, data,
-  } = useQuery('boxoffice', process.env.GQL_DEV === 'production' ? getBoxOffice : mock);
+  } = useQuery('boxoffice', process.env.NODE_ENV === 'production' ? getBoxOffice : mock);
 
   return useMemo(() => ({
     isLoading,
