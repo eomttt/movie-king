7bf744dd4a9cba777872744f7edb29ff4111f585
eomttt <hyunt0413@gmail.com>
2021-01-11 10:17:24 +0900

    Test code rollback

--- src/components/card/SearchedMovie/index.tsx
+++ src/components/card/SearchedMovie/index.tsx
@@ -1,6 +1,9 @@
 import { TheaterType } from 'constants/theater';
 import { SearchedMovieCard } from 'interfaces/card';
-import { useMemo } from 'react';
+import {
+  useEffect, useMemo, useRef, useState,
+} from 'react';
+import { createPortal } from 'react-dom';
 import { TheaterTypeColor } from 'styles/color';
 import * as Styles from './styles';
 
@@ -11,6 +14,7 @@ const SearchedMovie = ({
   title,
   time,
 }: SearchedMovieCard) => {
+  const iframeRef = useRef();
   const theaterType = useMemo(() => {
     if (type === TheaterType.CGV) {
       return 'CGV';
@@ -24,6 +28,8 @@ const SearchedMovie = ({
   return (
     <Styles.Container>
       <Styles.Content>
+        {/* <div dangerouslySetInnerHTML={{ __html: image }} /> */}
+        {/* <iframe src={image} title="image" ref={iframeRef} id="iframe" /> */}
         <img src={image} alt="Movie" onError={() => console.log('ERROR', title)} />
         <Styles.Menu>
           <Styles.MenuContent>
